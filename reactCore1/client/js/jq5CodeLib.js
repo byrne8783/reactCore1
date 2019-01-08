28909372792b5bfe3aa80656ac9ee1689f77b27c
byrne8783 <byrne8783@users.noreply.github.com>
2019-01-08 16:30:30 +0000

    forgot two

---
+++ reactCore1/client/js/jq5CodeLib.js
@@ -0,0 +1,10 @@
+﻿var JQ5Lib = (function () {
+    function JQ5Lib() {
+        this.textMessage = "Filled by jQuery from getData function in jq5Lib.js!";
+    }
+    JQ5Lib.prototype.getData = function () {
+        return this.textMessage;
+    };
+    return JQ5Lib;
+}());
+export default JQ5Lib;
